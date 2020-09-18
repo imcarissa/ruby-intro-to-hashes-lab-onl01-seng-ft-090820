f50b3331f88a4a7c0d2e9cff95e47ba7107b69b9
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-18 02:20:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,9 +26,14 @@ def monopoly_with_third_tier
 	                     pennsylvania_railroad: {},
 	                     b_and _o_railroad: {},
 	                     shortline: {},
-	      
-	                      }, 
-	                   rent_in_dollars: {one_piece_owned: 25, two_pieces_own: 50, three_pieces_owned: 100, four_pieces_owned: 200}
+	                 }, 
+	                   rent_in_dollars: {
+	                     one_piece_owned: 25,
+	                     two_pieces_own: 50, 
+	                     three_pieces_owned: 100,
+	                     four_pieces_owned: 200
+	                     
+	                 },
 	      
 	    }
 	}
