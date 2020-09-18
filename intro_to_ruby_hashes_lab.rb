27fc2acef640a95fb6a1198e8a06dd73bf4bbf35
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-18 02:45:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,7 +44,7 @@ def monopoly_with_fourth_tier
 	                   names: {
 	                           reading_railroad: {},
 	                           pennsylvania_railroad: {},
-	                           b_and _o_railroad: {},
+	                           b_and_o_railroad: {},
 	                           shortline: {},
 	                 }, 
 	                   rent_in_dollars: {
