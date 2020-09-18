ae76768536553ae1f4e859a98a675d4e66eb1579
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-18 02:59:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,7 +49,7 @@ monopoly = {
 	                  names: {
 	                    reading_railroad: {"mortgage_value" => "$100"},
 	                    pennsylvania_railroad: {"mortgage_value" => "$100"},
-	                    b_and_o_railroad: {},
+	                    b_and_o_railroad: {"mortgage_value" => "$100"},
 	                    shortline: {}
 	                  },
 	                  rent_in_dollars: {
