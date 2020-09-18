60a7bcb3a4f49dfc117b33cbcd125b6ffba8f0b3
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-18 02:44:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,8 +24,8 @@ end
 def monopoly_with_third_tier
 	monopoly = {
 	    :railroads => {
-	                  :pieces: 4, 
-	                   :names: {
+	                  :pieces => 4, 
+	                   :names => {
 	                     :reading_railroad => {},
 	                     :pennsylvania_railroad => {},
 	                     :b_and_o_railroad => {},
