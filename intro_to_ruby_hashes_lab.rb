d660594b1c8f4e174f734ac9c4ae1d236bebf2ce
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-18 02:03:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,9 +14,8 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	    :railroads => {pieces:}
+	    :railroads => {pieces: 4, names: {}, rent_in_dollars: {}}
 	}
-
 end
 
 def monopoly_with_third_tier
